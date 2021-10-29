37007790b6773c9e52a7528306147dd11a7b1628
Nazar <nazar.khort@gmail.com>
2021-10-29 23:32:17 +0300

    Created superuser and registered Article and Comment models in admin panel.

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from articles.models import Article, Comment
+
+
+@admin.register(Article)
+class ArticleAdmin(ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'author_name')
+
+
+@admin.register(Comment)
+class CommentAdmin(ModelAdmin):
+    list_display = ('id', 'author_name')
+    list_display_links = ('id',)
