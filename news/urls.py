5145464bed1c15a39f50e940d43e60b491c0884b
Nazar <nazar.khort@gmail.com>
2021-10-30 10:18:57 +0300

    Installed drf-nested-routers and imported it to urls.py. Created router as an instance of SimpleRouter, registered it and added as a base to nested router. Included all routers urls of to our urlpatterns. (Now on main page are visible list of urls. And we are able to use all of them, but without written templates. I suppose it is not necessary so I will try to add json response.)

--- news/urls.py
+++ news/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from articles.views import ArticleViewSet, CommentViewSet
+from rest_framework_nested import routers
+
+router = SimpleRouter()
+router.register('articles', ArticleViewSet)
+
+articles_router = routers.NestedSimpleRouter(router, r'articles', lookup='article')
+articles_router.register(r'comments', CommentViewSet, basename='article-comments')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include(articles_router.urls)),
 ]
+
+urlpatterns += router.urls
